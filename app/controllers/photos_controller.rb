833e348a5e0e70b618ecb7259f826c21563b5521
geh4y8 <guy.halperin88@gmail.com>
2014-09-14 12:09:15 -0700

    initial commit

---
+++ app/controllers/photos_controller.rb
@@ -0,0 +1,30 @@
+class PhotosController < ApplicationController
+  def index
+    @photos = Photo.all
+  end
+
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+
+    if @photo.save
+      redirect_to photo_path(@photo)
+    else
+      render'new'
+    end
+  end
+
+  def show
+    @photo = Photo.find(params[:id])
+    @result = @photo.identify
+  end
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:photo_choice, :name, :photo_url)
+  end
+end
---
+++ app/models/photo.rb
@@ -0,0 +1,26 @@
+class Photo < ActiveRecord::Base
+
+  has_attached_file :photo_choice, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default => ":rails_root/public/system/photos/no-image-available.png",
+  :url => ":rails_root/public/photos/images/:id/:style/:basename.:extension"
+  validates_attachment_content_type :photo_choice, :content_type => /\Aimage\/.*\Z/
+
+
+  after_create :identify
+
+  def identify
+    begin
+      # Cloudinary::Uploader.upload(photo_choice.url)
+      client = Rekognize::Client::Base.new(
+        api_key: 'd27krKEkBgGMGkSs',
+        api_secret: 'pfPCSFmsLPJtpU6t')
+      client.face_recognize(
+          urls: self.photo_url,
+          jobs: 'face_age face_beauty face_race face_emotion face_gender')
+    end
+  end
+
+ def photo_choice_url
+    JSON.parse("/Users/Guy/Documents/epicodus/doppelganger/public/system/photos/photo_choices/000/000/001/medium/bar.jpeg")
+end
+
+end
